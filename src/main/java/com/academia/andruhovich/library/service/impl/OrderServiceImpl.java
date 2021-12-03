87a72e484bb5533c5c3001465168d12d320e42d0
tolstiachok123 <sergey_andr1998@mail.ru>
2021-12-03 22:17:16 +0300

    Improved changelog_001.xml, improved create method in OrderServiceImpl

--- src/main/java/com/academia/andruhovich/library/service/impl/OrderServiceImpl.java
+++ src/main/java/com/academia/andruhovich/library/service/impl/OrderServiceImpl.java
@@ -11,8 +11,11 @@ import com.academia.andruhovich.library.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -24,22 +27,21 @@ public class OrderServiceImpl implements OrderService {
     private final BookRepository bookRepository;
     private final UserRepository repository;
 
+    @Transactional
     @Override
     public OrderDto create(OrderDto newOrderDto) {
         Order order = orderMapper.dtoToModel(newOrderDto);
 
-        Set<Book> books = new HashSet<>();
-        newOrderDto.getContent().keySet().forEach(id -> books.add(bookRepository.findById(id).get()));
-        final BigDecimal totalPrice = new BigDecimal("0.00");
+        List<Book> books = new ArrayList<>();
+        newOrderDto.getContent().keySet()
+                .forEach(id -> books.add(bookRepository.findById(id).get()));
         Order finalOrder = order;
-        books.forEach(book -> {
-            finalOrder.setTotalPrice(finalOrder.getTotalPrice().add(book.getPrice().multiply(new BigDecimal(newOrderDto.getContent().get(book.getId())))));
-        });
+        finalOrder.setTotalPrice(new BigDecimal("0.00"));
+        books.forEach(book -> finalOrder.setTotalPrice(finalOrder.getTotalPrice().add(book.getPrice().multiply(new BigDecimal(newOrderDto.getContent().get(book.getId()))))));
 
         order.setTotalPrice(finalOrder.getTotalPrice());
-        order = orderRepository.save(order);
-        OrderDto existingOrderDto = orderMapper.modelToDto(order);
-        return new OrderDto();//mock
+        order = orderRepository.save(order);    //toString change to json convert
+        return orderMapper.modelToDto(order);
     }
 
 }
